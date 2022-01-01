b1c7e824f7ec886dfbc9d6825c4e0dce68bccdb4
Hoai Bao <hoaibao.nhb@gmail.com>
2022-01-01 14:20:45 +0700

    first commit

---
+++ todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    processing = models.FloatField(default=0.0)
+    completed = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.title
